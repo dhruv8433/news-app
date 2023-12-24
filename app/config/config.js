45f98df673faf04b0741390c5ba25be4690a419b
dhruv8433 <dhruvrsoni04@gmail.com>
2023-12-24 18:49:09 +0530

    configurations

--- app/config/config.js
+++ app/config/config.js
@@ -1,1 +1,21 @@
-export const api_endpoint = `https://newsapi.org/v2`
+export const api_endpoint = `https://newsapi.org/v2`;
+
+export const categorys = [
+  "All",
+  "Technology",
+  "Sports",
+  "Business",
+  "Health",
+  "Family",
+  "Social",
+  "War",
+  "Education",
+  "Headlines",
+  "Car",
+  "Travel",
+  "Science",
+  "Religion",
+  "Politics",
+];
+
+export const key = "453bcc094cb84974bfca21c461508f0e";
