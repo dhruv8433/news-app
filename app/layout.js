3431d54fa16c7553016fda657160b641abdd643d
dhruv8433 <dhruvrsoni04@gmail.com>
2023-12-24 18:44:10 +0530

    layout

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,10 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "./components/NavBar";
-import '@/app/style/style.css'
+import "@/app/style/style.css";
 import { Toaster } from "react-hot-toast";
+import { ProviderStore } from "./StoreProvider";
+import Footer from "./components/Footer";
 require("dotenv").config();
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,11 +18,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div>
-          <NavBar />
-        </div>
-        {children}
-
+        <ProviderStore>
+          <div>
+            <NavBar />
+          </div>
+          {children}
+          <div>
+            <Footer />
+          </div>
+        </ProviderStore>
         <Toaster />
       </body>
     </html>
