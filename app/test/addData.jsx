1de4808a28c2eaabee40fb67eacc4a593ae6151c
dhruv8433 <dhruvrsoni04@gmail.com>
2023-12-24 18:49:29 +0530

    add data

---
+++ app/test/addData.jsx
@@ -0,0 +1,7 @@
+    // try {
+    //   const response = await AddUser(userData);
+    //   console.log(response);
+    //   if (response) alert("user created successfully");
+    // } catch (error) {
+    //   console.log(error);
+    // }
