a4e6a4ddae65313788895409333b4013aafa0cc1
dhruv8433 <dhruvrsoni04@gmail.com>
2023-12-24 18:46:24 +0530

    Navigation

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -4,29 +4,45 @@ import { Button, Container } from "@mui/material";
 import Link from "next/link";
 import React, { useState } from "react";
 import SignupForm from "./SignupForm";
+import { useRouter } from "next/navigation";
+import { Public } from "@mui/icons-material";
 
 const NavBar = () => {
   const [open, setOpen] = useState(false);
+  const [quert, setQuery] = useState("");
+  const router = useRouter();
 
+  const SpecificNewsSerached = (event) => {
+    if (event.key === "Enter") {
+      console.log("Enter Clicked");
+      router.push(`/query/${event.target.value}`);
+    }
+  };
   return (
     <div className="pb-3 primary-bg">
       <Container>
         <nav className="flex flex-row justify-between text-center items-center pt-3">
           <div className="left text-bold">
-            <Link href={"/"} className="text-2xl space-y-1" style={{}}>
-              ENews
+            <Link href={"/"} className="text-2xl space-y-1 text-white flex">
+              <div className="flex justify-center text-center items-center">
+                <Public />
+                <h1>ENews</h1>
+              </div>
             </Link>
           </div>
           <div className="right px-4">
-            <Link href={"/"}  className="text-white mx-2">
+            <Link href={"/"} className="text-white mx-2">
               Home
             </Link>
-            <Link href={"/"} className="text-white mx-2">
-              Top News
+            <Link href={"/popular-news"} className="text-white mx-2">
+              Popular
             </Link>
-            <Link href={"/"} className="text-white mx-2">
-              Live news
-            </Link>
+            <input
+              type="text"
+              className="mx-2 p-1 rounded"
+              placeholder="search anything"
+              onKeyPress={(e) => SpecificNewsSerached(e)}
+            />
             <Button
               variant="contained"
               onClick={() => setOpen(true)}
