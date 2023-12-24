b7473ff5427faf448dd5185dfae637d942561843
dhruv8433 <dhruvrsoni04@gmail.com>
2023-12-24 18:46:07 +0530

    news services

--- app/Services/getAllNews.js
+++ app/Services/getAllNews.js
@@ -1,8 +1,9 @@
+import { key } from "../config/config";
 import { httpAxios } from "../httpAxios";
 
 export const getSpecificQueryNews = async (q) => {
   const response = await httpAxios.get(
-    `/everything?q='${q}'&from=2023-11-23&sortBy=publishedAt&apiKey=b1eff6491ec94e23b32d5fd519fa0372`
+    `/everything?q='${q}'&from=2023-12-01&sortBy=publishedAt&apiKey=${key}`
   );
   return response.data;
 };
--- app/Services/getHeadlines.js
+++ app/Services/getHeadlines.js
@@ -1,8 +1,9 @@
+import { key } from "../config/config";
 import { httpAxios, topHeadlines } from "../httpAxios";
 
 export const getHeadlines = async () => {
   const response = await httpAxios.get(
-    `/top-headlines?country=us&apiKey=b1eff6491ec94e23b32d5fd519fa0372`
+    `/top-headlines?country=us&apiKey=${key}`
   );
   return response.data;
 };
