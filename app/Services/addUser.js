2fb6d63bfa97e91c3b098dad4b9971b761126c30
dhruv8433 <dhruvrsoni04@gmail.com>
2023-12-25 20:10:05 +0530

    add user to firestore

--- app/Services/addUser.js
+++ app/Services/addUser.js
@@ -1,20 +1,13 @@
 import { collection, addDoc } from "firebase/firestore";
 import { db } from "../firebaseConfig";
-import toast from "react-hot-toast";
 
 export async function AddUser(userData) {
   try {
     console.log("userData", userData);
 
-    const docRef = await addDoc(collection(db, "users"), {
-      email: userData.email,
-      password: userData.password,
-      phone: userData.phone,
-      name: userData.username,
-    });
+    const docRef = await addDoc(collection(db, "users"), userData);
     console.log("doc id", docRef.id);
 
-    toast.success("user created success!");
   } catch (error) {
     console.log(error);
   }
