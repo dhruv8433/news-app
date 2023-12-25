b46b868bcae06c5ebd1b9f9ed92a4af7130e1901
dhruv8433 <dhruvrsoni04@gmail.com>
2023-12-25 20:10:11 +0530

    google signup

--- app/Services/GoogleSignup.js
+++ app/Services/GoogleSignup.js
@@ -1,20 +1,40 @@
 import { signInWithPopup, signOut, GoogleAuthProvider } from "firebase/auth";
 import { auth } from "../firebaseConfig";
+import { AddUser } from "./addUser";
+import { UserLoginSuccess } from "../action/action";
+import toast from "react-hot-toast";
+import Cookies from "js-cookie";
 
-const googleSignIn = async () => {
+const googleSignIn = async ({ dispatch, setOpen }) => {
   const provider = new GoogleAuthProvider();
   const response = await signInWithPopup(auth, provider);
-  // this will print all user information in console for further processing 
+  // this will print all user information in console for further processing
   console.log("google response", response);
+  try {
+    const userObject = {
+      name: response.user.displayName,
+      email: response.user.email,
+      url: response.user.photoURL,
+    };
+    dispatch(UserLoginSuccess(userObject));
+    const userResponse = await AddUser(userObject);
+    Cookies.set("authenticated", true);
+    console.log("login success");
+    console.log(userResponse);
+    toast.success("user logged in successfully");
+    setOpen(false);
+  } catch (error) {
+    console.log("google error", error);
+  }
 };
 
 const logOut = () => {
   signOut(auth);
 };
 
-export async function handleSignIn() {
+export async function handleSignIn({ dispatch, setOpen }) {
   try {
-    await googleSignIn();
+    await googleSignIn({ dispatch, setOpen });
   } catch (error) {
     console.log(error);
   }
