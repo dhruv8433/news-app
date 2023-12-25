a2e423e3faa9d92f5622bf32357eb78744905f5b
dhruv8433 <dhruvrsoni04@gmail.com>
2023-12-25 20:09:57 +0530

    root reducer

--- app/reducer/rootReducer.js
+++ app/reducer/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { authReducer } from "./AuthReducer";
+import { favReducer } from "./FavReducer";
 
 export const rootReducer = combineReducers({
   auth: authReducer,
+  fav: favReducer,
 });
