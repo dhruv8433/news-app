0110d9f638306c121e87cbe8325a7fbc1197de08
dhruv8433 <dhruvrsoni04@gmail.com>
2024-01-17 09:26:48 +0530

    horizontal card

--- app/components/HorizontalCard.jsx
+++ app/components/HorizontalCard.jsx
@@ -54,80 +54,90 @@ const HorizontalCard = ({ data, profilePage }) => {
 
   return (
     <div>
-      {data.slice(0, 5).map((article) => (
-        <>
-          {/* if it doesn't a profile page than only this add fav btn visible */}
-          {!profilePage ? (
-            <div
-              className={`like absolute ml-2 mt-2 border bg-white rounded-full`}
-            >
-              <IconButton onClick={() => addToLike(article)}>
-                <FavoriteBorder />
-              </IconButton>
-            </div>
-          ) : (
-            ""
-          )}
-          <Link
-            key={article.title}
-            href={"/categorys/" + slugify(article.title).toLowerCase()}
-            onClick={() => saveDetails(article)}
-          >
-            <Card
-              sx={{ display: { xs: "block", md: "flex" } }}
-              className="justify-between my-4"
-            >
-              <CardMedia className="w-1/4 h-48">
-                <Box
-                  sx={{ width: { xs: "400px", md: "200px" } }}
-                  className="h-full"
-                >
-                  <img
-                    src={article.image}
-                    alt=""
-                    className="object-cover h-full"
-                    height={"100%"}
-                    width={"100%"}
-                  />
-                </Box>
-              </CardMedia>
-              <CardContent className="w-full">
-                <div className="text-start ml-3">
-                  <h1 className="hover:text-red-500 font-semibold">
-                    {article.title}
-                  </h1>
-                  <h1 className="hover:text-red-300 text-sm">
-                    {article.description}
-                  </h1>
-                  {article.author ? (
-                    <h1 className="text-sm mt-1 ">
-                      Discovered By:{" "}
-                      <span className="bg-red-500 w-max p-1 hover:text-white rounded">
-                        {article.author}
-                      </span>
-                    </h1>
-                  ) : (
-                    ""
-                  )}
-                </div>
-              </CardContent>
-            </Card>
-          </Link>
-          {/* inside profile page we give remove fav btn that remove any article from favoritesF */}
-          {profilePage ? (
-            <div className="flex justify-end mr-2 -mt-14">
-              <button
-                className="hover:bg-red-500 hover:text-white px-2 py-1 border border-gray-300 rounded"
-                onClick={() => RemoveFromFav(article)}
+      {data.slice(0, 5).map((article) => {
+        // to display ... after certain characters
+        const truncatedBody =
+          article.body.length > 300
+            ? `${article.body.substring(0, 300)}...`
+            : article.body;
+        return (
+          <>
+            {/* if it doesn't a profile page than only this add fav btn visible */}
+            {!profilePage ? (
+              <div
+                className={`like absolute ml-2 mt-2 border bg-white rounded-full`}
               >
-                Remove
-              </button>
-            </div>
-          ) : (
-            ""
-          )}
-        </>
-      ))}
+                <IconButton onClick={() => addToLike(article)}>
+                  <FavoriteBorder />
+                </IconButton>
+              </div>
+            ) : (
+              ""
+            )}
+            <Link
+              key={article.title}
+              href={"/categorys/" + slugify(article.title).toLowerCase()}
+              onClick={() => saveDetails(article)}
+            >
+              <Card
+                sx={{ display: { xs: "block", md: "flex" } }}
+                className="justify-between my-4"
+              >
+                <CardMedia className="w-1/4 h-48">
+                  <Box
+                    sx={{ width: { xs: "400px", md: "200px" } }}
+                    className="h-full"
+                  >
+                    <img
+                      src={article.image}
+                      alt=""
+                      className="object-cover h-full"
+                      height={"100%"}
+                      width={"100%"}
+                    />
+                  </Box>
+                </CardMedia>
+                <CardContent className="w-full">
+                  <div className="text-start ml-3">
+                    <h1 className="hover:text-red-500 font-semibold">
+                      {article.title}
+                    </h1>
+                    <h1 className="hover:text-red-300 text-sm">
+                      {truncatedBody}
+                    </h1>
+                    <h1 className="hover:text-red-400 text-sm">
+                      Date: {article.date}
+                    </h1>
+                    {article.author ? (
+                      <h1 className="text-sm mt-1 ">
+                        Discovered By:{" "}
+                        <span className="bg-red-500 w-max p-1 hover:text-white rounded">
+                          {article.author}
+                        </span>
+                      </h1>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                </CardContent>
+              </Card>
+            </Link>
+            {/* inside profile page we give remove fav btn that remove any article from favoritesF */}
+            {profilePage ? (
+              <div className="flex justify-end mr-2 -mt-14">
+                <button
+                  className="hover:bg-red-500 hover:text-white px-2 py-1 border border-gray-300 rounded"
+                  onClick={() => RemoveFromFav(article)}
+                >
+                  Remove
+                </button>
+              </div>
+            ) : (
+              ""
+            )}
+          </>
+        );
+      })}
     </div>
   );
 };
